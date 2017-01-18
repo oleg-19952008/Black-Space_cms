25281df449bb8781f18fc6d81fa9a12a6e889596
oleg-19952008 <o.borisychev@bravosoft.local>
2017-01-18 11:02:55 +0300

    swf

---
+++ spacemap/xml/gam.aspx.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.UI;
+//using System.Web.UI.WebControls;
+
+//public partial class scripts_screen_manager : System.Web.UI.Page
+//{
+//    protected void Page_Load(object sender, EventArgs e)
+//    {
+
+//    }
+//}
+using System;
+using System.Web;
+
+public partial class scripts_screen_manager : System.Web.UI.Page
+{
+    public static int n = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+   //     loading___ sq = new loading___();
+
+     //   var ss = System.IO.File.ReadAllLines(@"C:\f.t");
+        var x = Request.QueryString["x"];
+
+        var y = Request.QueryString["y"];
+
+        //if (Request.Params["width"] == "getResourceAsRaw")
+        //{
+      //  Response.Write(request);//getResourceAsRaw    
+      //  Response.Write(request_);     //}
+        var sql = new sql();
+       
+ 
+        //    sql.sql_exec_string("SELECT `screen_x`,`screen_y` FROM `users` WHERE  user_id = " + sql.user_id);  //"SELECT `screen_x`,`screen_y` FROM `users` WHERE  user_id = 1  ") //"SELECT * FROM `users` ORDER BY `login` ASC");
+     }
+}
